67bfcc40e4da76231e2d4a9e71157fda9266e6ad
Themathix <47466149+TheMathix@users.noreply.github.com>
2025-01-28 17:28:47 +0100

    trying to fix date

--- start.bat
+++ start.bat
@@ -3,8 +3,7 @@ call git reset --hard origin/main
 call git pull origin main
 %~dp0\terraria-server-1449\1449\Windows\TerrariaServer.exe -config %~dp0\terraria-server-1449\1449\Windows\serverconfig.txt
 call git add kOT*
-current="`date +'%Y-%m-%d %H:%M:%S'`"
-msg="Played: $current"
-call git commit -m "$msg"
+set msg="Played: %date% %time%"
+call git commit -m "%msg%"
 call git push origin main
 pause
