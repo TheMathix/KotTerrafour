b3651979593945a69699d080c9bc5182dd35c5c1
Themathix <47466149+TheMathix@users.noreply.github.com>
2025-01-28 17:06:19 +0100

    updated test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
+call git reset --hard origin/master
+call git pull origin master
 %~dp0\terraria-server-1449\1449\Windows\TerrariaServer.exe -config %~dp0\terraria-server-1449\1449\Windows\serverconfig.txt
-exit
+call git add *
+call git commit -m "Played: `date +'%Y-%m-%d %H:%M:%S'`"
+call git origin push master
 pause
